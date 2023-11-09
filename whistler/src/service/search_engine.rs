66a4f0113a00af74096851b1741c2d361ef10f06
Metal Whale <metalwhale.dev@gmail.com>
2023-11-09 04:58:54 +0000

    Handle cases when leaf query results contain only one item.

--- whistler/src/service/search_engine.rs
+++ whistler/src/service/search_engine.rs
@@ -47,8 +47,12 @@ pub(crate) async fn search_similar(sentence: String, embedding: Vec<f32>, limit:
     if lexical_results.len() > 0 {
         let (_, lexical_max_score) = lexical_results.first().context("Lexical max score")?.to_owned();
         let (_, lexical_min_score) = lexical_results.last().context("Lexical min score")?.to_owned();
-        for (id, score) in lexical_results {
-            let weighted_score = lexical_weight * (score - lexical_min_score) / (lexical_max_score - lexical_min_score);
+        for (id, score) in &lexical_results {
+            let weighted_score = if lexical_results.len() > 1 {
+                lexical_weight * (score - lexical_min_score) / (lexical_max_score - lexical_min_score)
+            } else {
+                lexical_weight
+            };
             weighted_lexical_results.insert(id, weighted_score);
         }
     }
@@ -57,19 +61,22 @@ pub(crate) async fn search_similar(sentence: String, embedding: Vec<f32>, limit:
     if semantic_results.len() > 0 {
         let (_, semantic_max_score) = semantic_results.first().context("Semantic max score")?.to_owned();
         let (_, semantic_min_score) = semantic_results.last().context("Semantic min score")?.to_owned();
-        for (id, score) in semantic_results {
-            let weighted_semantic_score =
-                semantic_weight * (score - semantic_min_score) / (semantic_max_score - semantic_min_score);
+        for (id, score) in &semantic_results {
+            let weighted_semantic_score = if semantic_results.len() > 1 {
+                semantic_weight * (score - semantic_min_score) / (semantic_max_score - semantic_min_score)
+            } else {
+                semantic_weight
+            };
             let score = if let Some(weighted_lexical_score) = weighted_lexical_results.remove(&id) {
                 weighted_lexical_score + weighted_semantic_score
             } else {
                 weighted_semantic_score
             };
-            compound_results.push((id, score));
+            compound_results.push((*id, score));
         }
     }
     for (id, score) in weighted_lexical_results {
-        compound_results.push((id, score));
+        compound_results.push((*id, score));
     }
     compound_results.sort_by(|(_, score1), (_, score2)| {
         score1
