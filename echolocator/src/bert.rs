f27f251107d62518c15e896707d9de5eb59aa566
Metal Whale <metalwhale.dev@gmail.com>
2023-11-10 12:51:07 +0000

    Refactor "echolocator".

--- echolocator/src/bert.rs
+++ echolocator/src/bert.rs
@@ -6,36 +6,51 @@ extern crate intel_mkl_src;
 #[cfg(feature = "accelerate")]
 extern crate accelerate_src;
 
-use anyhow::{Error, Result};
+use anyhow::{Error, Ok, Result};
 use candle_core::{DType, Device, Module, Tensor};
 use candle_nn::VarBuilder;
 use candle_transformers::models::jina_bert::{BertModel, Config};
 use hf_hub::{api::sync::Api, Repo, RepoType};
 use tokenizers::Tokenizer;
 
-pub(crate) fn build_model_and_tokenizer(model_name: String, tokenizer_name: String) -> Result<(BertModel, Tokenizer)> {
-    let model_path = Api::new()?
-        .repo(Repo::new(model_name, RepoType::Model))
-        .get("model.safetensors")?;
-    let tokenizer = Tokenizer::from_file(
-        Api::new()?
-            .repo(Repo::new(tokenizer_name, RepoType::Model))
-            .get("tokenizer.json")?,
-    )
-    .map_err(Error::msg)?;
-    let model = BertModel::new(
-        unsafe { VarBuilder::from_mmaped_safetensors(&[model_path], DType::F32, &Device::Cpu)? },
-        &Config::v2_base(),
-    )?;
-    Ok((model, tokenizer))
+const MODEL_NAME: &str = "jinaai/jina-embeddings-v2-base-en";
+const TOKENIZER_NAME: &str = "sentence-transformers/all-MiniLM-L6-v2";
+
+#[derive(Clone)]
+pub(crate) struct Bert {
+    model: BertModel,
+    tokenizer: Tokenizer,
 }
 
-pub(crate) fn embed(model: BertModel, mut tokenizer: Tokenizer, sentence: &str) -> Result<Vec<f32>> {
-    let tokenizer = tokenizer.with_padding(None).with_truncation(None).map_err(Error::msg)?;
-    let tokens = tokenizer.encode(sentence, true).map_err(Error::msg)?.get_ids().to_vec();
-    let token_ids = Tensor::new(&tokens[..], &model.device)?.unsqueeze(0)?;
-    let embedding = model.forward(&token_ids)?;
-    let (_sentences_num, tokens_num, _hidden_size) = embedding.dims3()?;
-    let embedding = (embedding.sum(1)? / (tokens_num as f64))?.squeeze(0)?;
-    Ok(embedding.to_vec1::<f32>()?)
+impl Bert {
+    pub(crate) fn new() -> Result<Self> {
+        let model_path = Api::new()?
+            .repo(Repo::new(MODEL_NAME.to_string(), RepoType::Model))
+            .get("model.safetensors")?;
+        let tokenizer = Tokenizer::from_file(
+            Api::new()?
+                .repo(Repo::new(TOKENIZER_NAME.to_string(), RepoType::Model))
+                .get("tokenizer.json")?,
+        )
+        .map_err(Error::msg)?;
+        let model = BertModel::new(
+            unsafe { VarBuilder::from_mmaped_safetensors(&[model_path], DType::F32, &Device::Cpu)? },
+            &Config::v2_base(),
+        )?;
+        return Ok(Self { model, tokenizer });
+    }
+
+    pub(crate) fn embed(&mut self, sentence: &str) -> Result<Vec<f32>> {
+        let tokenizer = self
+            .tokenizer
+            .with_padding(None)
+            .with_truncation(None)
+            .map_err(Error::msg)?;
+        let tokens = tokenizer.encode(sentence, true).map_err(Error::msg)?.get_ids().to_vec();
+        let token_ids = Tensor::new(&tokens[..], &self.model.device)?.unsqueeze(0)?;
+        let embedding = self.model.forward(&token_ids)?;
+        let (_sentences_num, tokens_num, _hidden_size) = embedding.dims3()?;
+        let embedding = (embedding.sum(1)? / (tokens_num as f64))?.squeeze(0)?;
+        Ok(embedding.to_vec1::<f32>()?)
+    }
 }
--- echolocator/src/main.rs
+++ echolocator/src/main.rs
@@ -3,25 +3,22 @@ mod llama;
 
 use std::env;
 
-use anyhow::{Context, Error, Result};
+use anyhow::{Error, Result};
 use axum::{
     extract::State,
     response::{IntoResponse, Response},
     routing, Json, Router,
 };
-use candle_transformers::models::jina_bert::BertModel;
-use futures_util::StreamExt;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use tokenizers::Tokenizer;
-use tokio::{fs::File, io::AsyncWriteExt};
+
+use crate::bert::Bert;
+use crate::llama::Llama;
 
 #[derive(Clone)]
 struct AppState {
-    llama_model_path: String,
-    llama_instruct_template: String,
-    bert_model: BertModel, // TODO: Need a type that is more general
-    bert_tokenizer: Tokenizer,
+    llama: Llama,
+    bert: Bert,
 }
 
 // See: https://github.com/tokio-rs/axum/blob/c979672/examples/anyhow-error-response/src/main.rs#L34-L57
@@ -46,9 +43,8 @@ where
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let model_dir = "/usr/src/models".to_string();
-    let state = initialize(&model_dir).await?;
-    println!("[INFO] main: state.llama_model_path={}", &state.llama_model_path);
+    let state = initialize().await?;
+    println!("[INFO] main: initialize");
     let app = Router::new()
         .route("/healthz", routing::get(|| async { "Ok" }))
         .route("/instruct", routing::post(instruct))
@@ -62,41 +58,10 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn initialize(model_dir: &str) -> Result<AppState> {
-    // Llama
-    let llama_model_url = env::var("ECHOLOCATOR_LLAMA_MODEL_URL").unwrap_or(
-        "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q5_K_M.gguf".to_string(),
-    );
-    let llama_instruct_template =
-        env::var("ECHOLOCATOR_LLAMA_INSTRUCT_TEMPLATE").unwrap_or("<s>[INST] {instruction} [/INST]".to_string());
-    let llama_model_name = llama_model_url
-        .split("/")
-        .last()
-        .context(format!("model_url={llama_model_url}"))?
-        .to_string();
-    let llama_model_path = format!("{model_dir}/{llama_model_name}");
-    // Bert
-    let bert_model_name =
-        env::var("ECHOLOCATOR_BERT_MODEL_NAME").unwrap_or("jinaai/jina-embeddings-v2-base-en".to_string());
-    let bert_tokenizer_name =
-        env::var("ECHOLOCATOR_BERT_TOKENIZER_NAME").unwrap_or("sentence-transformers/all-MiniLM-L6-v2".to_string());
-    let (bert_model, bert_tokenizer) = bert::build_model_and_tokenizer(bert_model_name, bert_tokenizer_name)?;
-    // App state
-    let state = AppState {
-        llama_model_path,
-        llama_instruct_template,
-        bert_model,
-        bert_tokenizer,
-    };
-    if std::path::Path::new(&state.llama_model_path).exists() {
-        return Ok(state);
-    }
-    let mut file = File::create(&state.llama_model_path).await?;
-    let mut stream = reqwest::get(llama_model_url).await?.bytes_stream();
-    while let Some(chunk) = stream.next().await {
-        file.write_all(&chunk?).await?;
-    }
-    file.flush().await?;
+async fn initialize() -> Result<AppState> {
+    let llama = Llama::new().await?;
+    let bert = Bert::new()?;
+    let state = AppState { llama, bert };
     Ok(state)
 }
 
@@ -114,21 +79,8 @@ async fn instruct(
     State(state): State<AppState>,
     Json(payload): Json<InstructRequest>,
 ) -> Result<Json<InstructResponse>, AppError> {
-    let prompt = state
-        .llama_instruct_template
-        .replace("{instruction}", &payload.instruction);
-    let completion = llama::inference(&state.llama_model_path, &prompt).await?;
-    let response = InstructResponse {
-        completion: completion
-            // TODO: Prevent the output of the prompt rather than having to manually remove it from the completion
-            .replace(
-                // prompt lacks both BOS and EOS markers in the completion
-                &prompt.replace("<s>", "").replace("</s>", ""),
-                "",
-            )
-            .trim()
-            .to_string(),
-    };
+    let completion = state.llama.inference(&payload.instruction).await?;
+    let response = InstructResponse { completion };
     Ok(Json(response))
 }
 
@@ -143,10 +95,10 @@ struct EmbedResponse {
 }
 
 async fn embed(
-    State(state): State<AppState>,
+    State(mut state): State<AppState>,
     Json(payload): Json<EmbedRequest>,
 ) -> Result<Json<EmbedResponse>, AppError> {
-    let embedding = bert::embed(state.bert_model, state.bert_tokenizer, &payload.sentence)?;
+    let embedding = state.bert.embed(&payload.sentence)?;
     let response = EmbedResponse { embedding };
     Ok(Json(response))
 }
