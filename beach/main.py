de4e7e3e88f72b2829c8e56ff188bc6b2da61436
Metal Whale <metalwhale.dev@gmail.com>
2023-12-01 14:18:00 +0000

    Add embedder fine tuning notebook.

--- beach/main.py
+++ beach/main.py
@@ -1,80 +1,12 @@
-import csv
 import json
 import os
-import sys
+from pathlib import Path
 
 import psycopg2
 
 
-def export_texts(cursor: psycopg2.extensions.cursor, data_dir: str, chunk_size: int = 50):
-    cursor.execute("SELECT count(*) FROM item_urls WHERE text IS NOT NULL")
-    total, *_ = cursor.fetchone()
-    print(f"Total: {total}")
-    # chunk_index = total // chunk_size  # Temporary
-    chunk_index = 0
-    texts_dir = os.path.join(data_dir, "texts")
-    os.makedirs(texts_dir, exist_ok=True)
-    with open(os.path.join(data_dir, "items.csv"), "w") as items_file:
-        items_writer = csv.DictWriter(items_file, fieldnames=["id", "url", "title"])
-        items_writer.writeheader()
-        while True:
-            print(f"[INFO] chunk_index={chunk_index}")
-            offset = chunk_index * chunk_size
-            cursor.execute(
-                "SELECT id, url, title, item_urls.text "
-                "FROM items "
-                "JOIN item_urls ON items.id = item_urls.item_id "
-                "WHERE item_urls.text IS NOT NULL "
-                "ORDER BY id DESC "
-                f"OFFSET {offset} LIMIT {chunk_size}"
-            )
-            results = cursor.fetchall()
-            if len(results) == 0:
-                break
-            for row in results:
-                item_id, url, title, text = row
-                items_writer.writerow({"id": item_id, "url": url, "title": title})
-                with open(os.path.join(texts_dir, f"{item_id}.txt"), "w") as text_file:
-                    text_file.write(text)
-            items_file.flush()
-            chunk_index += 1
-
-
-def export_summaries(cursor: psycopg2.extensions.cursor, data_dir: str, chunk_size: int = 50):
-    cursor.execute("SELECT count(*) FROM item_urls WHERE summary IS NOT NULL")
-    total, *_ = cursor.fetchone()
-    print(f"Total: {total}")
-    chunk_index = 0
-    summaries_dir = os.path.join(data_dir, "summaries")
-    os.makedirs(summaries_dir, exist_ok=True)
-    with open(os.path.join(data_dir, "items.csv"), "w") as items_file:
-        items_writer = csv.DictWriter(items_file, fieldnames=["id", "title"])
-        items_writer.writeheader()
-        while True:
-            print(f"[INFO] chunk_index={chunk_index}")
-            offset = chunk_index * chunk_size
-            cursor.execute(
-                "SELECT id, title, summary "
-                "FROM items "
-                "JOIN item_urls ON items.id = item_urls.item_id "
-                "WHERE summary IS NOT NULL "
-                "ORDER BY id DESC "
-                f"OFFSET {offset} LIMIT {chunk_size}"
-            )
-            results = cursor.fetchall()
-            if len(results) == 0:
-                break
-            for row in results:
-                item_id, title, summary = row
-                items_writer.writerow({"id": item_id, "title": title})
-                with open(os.path.join(summaries_dir, f"{item_id}.txt"), "w") as summary_file:
-                    summary_file.write(summary)
-            items_file.flush()
-            chunk_index += 1
-
-
 def export_passages(cursor: psycopg2.extensions.cursor, data_dir: str, chunk_size: int = 50):
-    cursor.execute("SELECT count(*) FROM analyses WHERE text_passage IS NOT NULL")
+    cursor.execute("SELECT count(*) FROM analyses WHERE text_passage IS NOT NULL OR summary_passage IS NOT NULL")
     total, *_ = cursor.fetchone()
     print(f"Total: {total}")
     chunk_index = 0
@@ -83,10 +15,11 @@ def export_passages(cursor: psycopg2.extensions.cursor, data_dir: str, chunk_siz
         print(f"[INFO] chunk_index={chunk_index}")
         offset = chunk_index * chunk_size
         cursor.execute(
-            "SELECT id, text, text_passage "
+            "SELECT id, items.text, item_urls.summary, text_passage, summary_passage "
             "FROM items "
+            "LEFT JOIN item_urls ON items.id = item_urls.item_id "
             "LEFT JOIN analyses ON items.id = analyses.item_id "
-            "WHERE text_passage IS NOT NULL "
+            "WHERE text_passage IS NOT NULL OR summary_passage IS NOT NULL "
             "ORDER BY id DESC "
             f"OFFSET {offset} LIMIT {chunk_size}"
         )
@@ -94,28 +27,27 @@ def export_passages(cursor: psycopg2.extensions.cursor, data_dir: str, chunk_siz
         if len(results) == 0:
             break
         for row in results:
-            item_id, text, text_passage = row
+            item_id, text, summary, text_passage, summary_passage = row
             with open(os.path.join(data_dir, f"{item_id}.json"), "w") as item_file:
+                if text_passage is not None:
+                    content = text
+                    passage = text_passage
+                elif summary_passage is not None:
+                    content = summary
+                    passage = summary_passage
+                else:
+                    continue
                 item_data = {
-                    "text": text,
-                    "text_passage": json.loads(text_passage),
+                    "content": content,
+                    "passage": json.loads(passage),
                 }
                 item_file.write(json.dumps(item_data, indent=4))
         chunk_index += 1
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python main.py <COMMAND> ./data/")
-        exit(1)
-    command, data_dir = sys.argv[1:]
     connection = psycopg2.connect(os.environ["DATABASE_URL"])
     cursor = connection.cursor()
-    if command == "texts":
-        export_texts(cursor, data_dir)
-    elif command == "summaries":
-        export_summaries(cursor, data_dir)
-    elif command == "passages":
-        export_passages(cursor, data_dir)
+    export_passages(cursor, os.path.join(Path(__file__).parent, "data"))
     cursor.close()
     connection.close()
